35bac4a48bc6c22a1a1f4dc07d85f6f56e686576
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-10-15 00:08:13 +0200

    bucle para pintar personajes, filtro por nombre y filtro por casa

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import getCharacters from "../services/api";
 
 const HomePage = () =>{
     const [characters, setCharacters] = useState([]);
+    const [name, setName] = useState("");
+    const [house, setHouse] = useState("");
 
     useEffect(() => {
         getCharacters().then(data =>{
@@ -11,12 +13,51 @@ const HomePage = () =>{
         })
   }, []);
 
+        const handleChangeName = (ev) => {
+        setName (ev.target.value);
+    }
+
+        const handleChangeHouse = (ev) => {
+        setHouse (ev.target.value);
+    }
+
+    const getHouses = () => {
+    const characterHouses = characters
+      .map((character) => character.house)
+      .filter((h) => h && h.trim() !== "");
+    return [...new Set(characterHouses)];
+    };
+
     return (
         <>
+
+            <label htmlFor="name">Busca por personaje: </label>
+            <input name="name" id="name" value={name} onChange={handleChangeName}/>
+            <label htmlFor="house">
+                Selecciona la casa:
+                <select name="house" id="house" value={house} onChange={handleChangeHouse}>
+                    <option value="">Todas</option>
+                    {getHouses().map((housesName, index) => (
+                        <option key={index} value={housesName}>
+                            {housesName}
+                        </option>
+                    ))}
+                </select>
+            </label>
+
             <ul>
-                {characters.map((character, id) => (
+                {characters
+                .filter((character) =>
+                    character.name.toLowerCase().includes(name.toLowerCase()))
+                .filter((character) => {
+                    if (!house) return true;
+                    return (character.house || "").toLowerCase() === house.toLowerCase();
+                })
+                .map((character, id) => (
                     <li key={id}>
-                        {character.name}
+                        <p>{character.name}</p>
+                        {character.image ? <img src={character.image} alt={character.name}/> : <img src={"https://media.desenio.com/site_images/68631b0f92c536b9cc92b033_1852152599_WB0012-5.jpg"} />}
+                        <p>{character.gender} | {character.species} | {character.house}</p>
                     </li>
                 ))}
             </ul>
