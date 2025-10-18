7e6d7ae93a62a5bff9d14035694732732f8f49f7
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-10-18 11:53:18 +0200

    ultimos detalles css

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
     return (
         <ul>
-            <li><Link to="/">El personaje que buscas no existe, ir a la Home</Link></li>
+            <Link to="/">El personaje que buscas no existe, ir a la Home</Link>
         </ul>
     )
 }
